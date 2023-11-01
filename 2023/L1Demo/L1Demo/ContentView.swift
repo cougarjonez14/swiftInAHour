58018e034718cf3db9345b43c4ba89e572904ff0
stationAstral <cougarjonez@gmail.com>
2023-11-01 11:34:34 -0700

    1.1

---
+++ 2023/L1Demo/L1Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L1Demo
+//
+//  Created by stationAstral on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2023/L1Demo/L1Demo/L1DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1DemoApp.swift
+//  L1Demo
+//
+//  Created by stationAstral on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct L1DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
