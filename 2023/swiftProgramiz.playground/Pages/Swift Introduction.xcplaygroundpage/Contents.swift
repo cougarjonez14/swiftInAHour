b0c509fd86fad9502e70c4dcacc8bfe1ba22139b
stationAstral <cougarjonez@gmail.com>
2023-11-21 21:00:20 -0800

    1

--- 2023/swiftProgramiz.playground/Pages/Swift Introduction.xcplaygroundpage/Contents.swift
+++ 2023/swiftProgramiz.playground/Pages/Swift Introduction.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
-//: [Previous](@previous)
+// Swift Hello World Program
+print("Hello, World!")
 
-import Foundation
 
-var greeting = "Hello, playground"
+// A variable is a container (storage area) to hold data
+var num = 10
 
-//: [Next](@next)
+
+// In swift we cannot change the type of the variable once it is declared
+var a: String
+var b: Int
+
+
+// Assigning values to variables
+a = "Jameo"
+b = 4
+
+
+// A constant is a special type of variable whose value cannot be changed
+let c = 5
+
+
---
+++ swiftful1.playground/Pages/ Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+// this is a comment
+
+/* 
+ this is a
+ multi-line comment
+ */
+
+
+//
+
+var thisIsCamelCase = 1
+
