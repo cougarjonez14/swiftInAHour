fe9388a6967346ed6fe3fbf710922714879be5e9
stationAstral <cougarjonez@gmail.com>
2023-12-14 23:32:10 -0800

    1

---
+++ 2023/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by stationAstral on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
